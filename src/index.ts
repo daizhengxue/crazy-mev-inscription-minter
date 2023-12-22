3c968a66c69ab6afafbe874272301e988dcc8115
daizhengxue <daizhengxue828@gmail.com>
2023-12-22 18:19:49 +0800

    updata

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,9 @@ import 'dotenv/config';
 import { decode } from "punycode";
 
 const CHAIN_ID = 5;//for goerli
-const provider = new providers.InfuraProvider(CHAIN_ID,process.env.API_KEY)
+//const provider = new providers.InfuraProvider(CHAIN_ID,process.env.API_KEY)
 //const provider = new providers.JsonRpcProvider(`https://ethereum-goerli.s.chainbase.online/v1/${process.env.Chainbase_API_KEY}`);
+const provider = new providers.JsonRpcProvider(`https://rpc.ankr.com/eth_goerli`);
 
 const FLASHBOTS_ENDPOINT = "https://relay-goerli.flashbots.net";
 
@@ -14,23 +15,23 @@ if (process.env.WALLET_PRIVATE_KEY === undefined) {
   process.exit(1)
 }
 const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY, provider)
-
 // ethers.js can use Bignumber.js class OR the JavaScript-native bigint. I changed this to bigint as it is MUCH easier to deal with
 const GWEI = 10n ** 9n;
-//const ETHER = 10n ** 18n;
+//const ETHER = 10n ** 18n; 
  //data: `data:,{"p":"erc-20","op":"mint","tick":"mev","amt":"1000"}`
- //0x646174613a2c7b2270223a226572632d3230222c226f70223a226d696e74222c227469636b223a226d6576222c22616d74223a2231303030227d
- //const to_address = "0x31761aA5BDDAFCb6B39D3EA69043C24096Fe5ddC";
+ const inscribe = "0x646174613a2c7b2270223a226572632d3230222c226f70223a226d696e74222c227469636b223a226d6576222c22616d74223a2231303030227d";
+ const to_address = "0x31761aA5BDDAFCb6B39D3EA69043C24096Fe5ddC";
  //replace the inscribe data and to address with your own
-const inscribe = "Input your data here";
-const to_address = "input your address here";
+
+//const inscribe = "Input your data here";
+//const to_address = "input your address here";
 
 async function main() {
   const flashbotsProvider = await FlashbotsBundleProvider.create(provider, Wallet.createRandom(), FLASHBOTS_ENDPOINT)
   provider.on('block', async (blockNumber) => {
     console.log(blockNumber)
 
-// input your transaction here and change the data into hex in this case
+// input your transaction here and you can try input as much as you want for example you can input 10 transactions in one bundle
 // also change the to address to the contract address you want to interact with
     const bundleSubmitResponse = await flashbotsProvider.sendBundle(
       [
